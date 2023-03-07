a51bebf122b75c1a8acd5aa98b46fa0732046576
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2023-03-07 14:24:54 -0600

    feat: import css from antd

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import store from "../store";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "bootstrap/dist/css/bootstrap-utilities.min.css";
 import "antd/dist/reset.css";
+import "../node_modules/antd/lib"
 import "../styles/globals.css";
 import "animate.css";
 import "../styles/animations.css";
